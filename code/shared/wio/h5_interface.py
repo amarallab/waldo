a30c1090565b547dca052e7536630a7a3ddbddc3
peter winter <peterwinteriii@gmail.com>
2014-02-27 08:52:57 -0600

    h5py interface appears to be working using tests.

--- code/shared/wio/h5_interface.py
+++ code/shared/wio/h5_interface.py
@@ -17,7 +17,7 @@ import json
 from glob import glob
 from itertools import izip
 import h5py
-
+import numpy as np
 '''
 # path definitions
 HERE = os.path.dirname(os.path.realpath(__file__))
@@ -31,10 +31,50 @@ sys.path.append(SHARED_DIR)
 OPTIONS = {}
 
 def write_h5_timeseries_base(h5_file, h5_path, times, data):
-    pass
-
+    times = np.array(times) 
+    data = np.array(data)
+    
+    with h5py.File(h5_file, 'w') as f:
+        grp = f.create_group(h5_path)
+        #print times.shape
+        grp.create_dataset(name='time',                           
+                           shape=times.shape,
+                           dtype=float,
+                           data=times,
+                           chunks=True,
+                           compression='lzf')
+        grp.create_dataset(name='data',                           
+                           shape=data.shape,
+                           dtype=float,
+                           data=data,
+                           chunks=True,
+                           compression='lzf')
+        
+                                                               
 def read_h5_timeseries_base(h5_file, h5_path):
     times, data = [], []
+    path_parts = map(unicode, h5_path.split('/'))
+    #print path_parts
+    with h5py.File(h5_file, 'r') as f:
+        #times_path = '{p}/time'.format(p=h5_path.rstrip('/'))
+        #data_path = '{p}/data'.format(p=h5_path.rstrip('/'))        
+        part1, parts = path_parts[0], path_parts[1:]
+        if part1 in f.keys():
+            grp = f[part1]    
+            for i, p in enumerate(parts):
+                #print i, p
+                if p in grp.keys():
+                    grp = grp[p]
+                
+            times = np.array(grp['time'])
+            data = np.array(grp['data'])
+            #data = np.array(f[data_path])            
+                    
+        #print grp.keys()
+        #print times_path
+        #print data_path
+        
+
     return times, data
 
 def delete_h5_dataset(h5_file, h5_path, times, data):
