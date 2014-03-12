fdccc12923fa04abe629ab83806959e6f954e04e
peter winter <peterwinter2013@u.northwestern.edu>
2014-03-11 19:40:23 -0500

    changed local user info

--- code/settings/local.py
+++ code/settings/local.py
@@ -9,20 +9,24 @@ from base import *
 
 import getpass
 
-# passwords
-MONGO['user'] = get_env_variable('WORM_MONGO_USER')
-MONGO['password'] = get_env_variable('WORM_MONGO_PASSWORD')
-SPREADSHEET['user'] = get_env_variable('WORM_SPREADSHEET_USER')
-SPREADSHEET['password'] = get_env_variable('WORM_SPREADSHEET_PASSWORD')
-
 # testing sheets
 SPREADSHEET['spreadsheet'] = 'testan'
 SPREADSHEET['scaling-factors'] = 'testsf'
 
 username = getpass.getuser()
-
 if username == 'heltena':
+    # passwords
+    MONGO['user'] = get_env_variable('WORM_MONGO_USER')
+    MONGO['password'] = get_env_variable('WORM_MONGO_PASSWORD')
+    SPREADSHEET['user'] = get_env_variable('WORM_SPREADSHEET_USER')
+    SPREADSHEET['password'] = get_env_variable('WORM_SPREADSHEET_PASSWORD')
+
     LOGISTICS['filesystem_data'] = '/Users/heltena/src/waldo/data/heltena/'
 
-# elif username == 'peterwinter':
-#...
+elif username == 'peterwinter':
+    # passwords
+    MONGO['user'] = get_env_variable('WORM_MONGO_USER')
+    MONGO['password'] = get_env_variable('WORM_MONGO_PASSWORD')
+    SPREADSHEET['user'] = get_env_variable('WORM_SPREADSHEET_USER')
+    SPREADSHEET['password'] = get_env_variable('WORM_SPREADSHEET_PASSWORD')
+
