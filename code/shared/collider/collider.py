b51a3b1c48573f1eee9c44ec750e8895c9f5c9a9
Nicholas Timkovich <npt@u.northwestern.edu>
2014-05-21 20:48:53 -0500

    Rough in basic "chain" removing using tests to verify expected results

--- code/shared/collider/collider.py
+++ code/shared/collider/collider.py
@@ -32,23 +32,23 @@ def check_assumptions(graph):
             assert graph.out_degree(predecessors[0]) == 2, "Fusion parent has unexpected number of children (not 2)"
 
 
-def remove_chains(graph):
+def remove_chains(graph, conditional=None):
     """
     Strip out fission-fusion events (and repetitions thereof) from the
     graph.
 
     ..
-          ~~~
-           |
-           A
-          / \           ~~~
-         /   \           |
-        B     C   ==>   A-D
-         \   /           |
-          \ /           ~~~
-           D
-           |
-          ~~~
+           ~~~
+            |
+            A
+           / \           ~~~
+          /   \           |
+         B     C   ==>   A-D
+          \   /           |
+           \ /           ~~~
+            D
+            |
+           ~~~
 
     The hidden data will be attached to the nodes in a simple list, for
     example from above: ``[A, (B, C), D]``.
@@ -57,4 +57,39 @@ def remove_chains(graph):
     all_nodes = graph.nodes()
 
     for node in all_nodes:
-        pass#if node.wat
+        if node not in graph:
+            continue # node was already removed/abridged
+
+        parents = set(graph.predecessors(node))
+        children = set(graph.successors(node))
+        grandchildren = set()
+        for child in children:
+            grandchildren.update(graph.successors_iter(child))
+
+        if len(grandchildren) != 1:
+            continue # skip doing anything
+
+        if conditional is not None:
+            if conditional(graph, children):
+                pass
+            else:
+                continue # children fail conditional testing
+
+        grandchild = grandchildren.pop()
+        greatgrandchildren = set(graph.successors(grandchild))
+
+        cnode = '{}-{}'.format(node, grandchild)
+        cnode_data = {'components': [node, children, grandchild]}
+
+        graph.add_node(cnode, **cnode_data)
+        graph.add_edges_from((p, cnode) for p in parents)
+        graph.add_edges_from((cnode, ggc) for ggc in greatgrandchildren)
+
+        graph.remove_node(node)
+        graph.remove_nodes_from(children)
+        graph.remove_node(grandchild)
+
+    #return graph
+
+
+
