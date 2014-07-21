8bff76e7a0fefb4aafa5a144ce64aabf47c3eb1f
Heltena <heltena@gmail.com>
2014-07-21 11:32:21 -0500

    Subgraph again in init (collider)

--- code/shared/collider/__init__.py
+++ code/shared/collider/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 
 # from . import viz # require manual import
 from .simplifications import *
-#from . import subgraph  #WHERE DID DUBGRAPH MOVE TO?
+from . import subgraph
