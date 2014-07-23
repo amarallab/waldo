074a1b6847878c601ed0c1c6fdf9780c376d83ac
Heltena <heltena@gmail.com>
2014-07-23 15:59:57 -0500

    save fingerprint (only for one frame)

--- code/heltena/test_idtracker.py
+++ code/heltena/test_idtracker.py
@@ -2,8 +2,9 @@ from PIL import Image
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import sys
 
-def calculate_fingerprint(image_filename, mask_filename):
+def calculate_fingerprint(image_filename, mask_filename, output_filename):
     _image = Image.open(image_filename)
     _mask = Image.open(mask_filename)
     image = np.array(_image.getdata())
@@ -27,9 +28,12 @@ def calculate_fingerprint(image_filename, mask_filename):
 #         if ix == 0:
 #             print ""
 
-    plus_scores = []
-    minus_scores = []
-    dist_scores = []
+    MAX_PLUS = 2.0
+    MAX_MINUS = 1.0
+    MAX_DIST = 80.0
+    plus_scores = [MAX_PLUS]
+    minus_scores = [MAX_MINUS]
+    dist_scores = [MAX_DIST]
     ix, iy = 0, 0
     for i in range(len(image)-1):
         iv = image[i]
@@ -38,37 +42,32 @@ def calculate_fingerprint(image_filename, mask_filename):
             for j in range(i+1, len(image)):
                 jv = image[j]
                 if jv > 0:
-                    plus_ij = iv * iv + jv * jv
-                    minus_ij = abs(iv * iv - jv * jv)
                     dist = math.hypot(jx-ix, jy-iy)
-                
-                    plus_scores.append(plus_ij)
-                    minus_scores.append(minus_ij)
+                    if dist > MAX_DIST:
+                        print "E: dist greater than maximum (%f > %f)" % (dist, MAX_DIST)
+                    plus_scores.append(iv + jv)
+                    minus_scores.append(abs(iv - jv))
                     dist_scores.append(dist)
-                
                 jx, jy = move_pixel(jx, jy)
         ix, iy = move_pixel(ix, iy)
-
-    plus_heatmap, xedges, yedges = np.histogram2d(dist_scores, plus_scores, bins=50)
-    plus_extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-
-    minus_heatmap, xedges, yedges = np.histogram2d(dist_scores, minus_scores, bins=50)
-    minus_extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-
+    
+    plus_heatmap, _, _ = np.histogram2d(dist_scores, plus_scores, bins=(32,32))
+    minus_heatmap, _, _ = np.histogram2d(dist_scores, minus_scores, bins=(32,32))
     f, ax = plt.subplots(1,2)
     f.set_size_inches((6,3))
-    ax[0].imshow(plus_heatmap, extent=plus_extent, aspect='auto')
-    ax[1].imshow(minus_heatmap, extent=minus_extent, aspect='auto')
+    ax[0].imshow(plus_heatmap, vmin=0, vmax=1024)
+    ax[1].imshow(minus_heatmap, vmin=0, vmax=1024)
+    f.savefig(output_filename)
 
+def main(args):
+    for arg in args:
+        if not arg.endswith("_img.png"):
+            print "E: file '%s' cannot be treated"
+        else:
+            image_filename = arg
+            mask_filename = arg[0:-len("_img.png")] + "_mask.png"
+            fingerprint_filename = arg[0:-len("_img.png")] + "_fingerprint.png"
+            calculate_fingerprint(image_filename, mask_filename, fingerprint_filename)
 
-def calculate_from_id(id):
-    BASEDIR = "/Users/heltena/src/waldo/data/cutouts/20130702_135704"
-    TEST_BASE_NAME = "1167_0/{id}".format(id=id)
-    TEST_IMAGE_FILENAME = "{basedir}/{basename}_img.png".format(basedir=BASEDIR, basename=TEST_BASE_NAME)
-    TEST_MASK_FILENAME =  "{basedir}/{basename}_mask.png".format(basedir=BASEDIR, basename=TEST_BASE_NAME)
-    calculate_fingerprint(TEST_IMAGE_FILENAME, TEST_MASK_FILENAME)
-    
-calculate_from_id(11)
-calculate_from_id(15)
-calculate_from_id(81)
-calculate_from_id(83)
+if __name__ == "__main__":
+    main(sys.argv[1:])
