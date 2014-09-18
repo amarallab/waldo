68291418f9da27cc524abb7e175259fa3b3bd734
Heltena <heltena@gmail.com>
2014-09-18 09:57:08 -0500

    collapse_group_of_nodes bug fixed... again

--- code/shared/collider/simplifications/intrinsic/collapse_group_of_nodes.py
+++ code/shared/collider/simplifications/intrinsic/collapse_group_of_nodes.py
@@ -9,71 +9,96 @@ __author__ = 'heltena'
 # One the candidate list are completed, the algorithm loops the list ordered by inverse length (bigger first). For
 # each candidate, remove from the remain list all the lists that contains at least one of the nodes in the candidate.
 # The algorithm loops until no candidates found.
-def collapse_group_of_nodes(graph, max_duration, debug=False):
-    while True:
-        candidates = []
-        for node in graph.nodes():
-            born = graph.node[node]['died_t']
-            last_died = graph.node[node]['died_t']
-            remain = graph.successors(node)
-            current_group = [node]
-            last_safe = None
-            while len(remain) > 0 and last_died - born < max_duration:
-                remain = sorted(remain, key=lambda x: graph.node[x]['born_t'])
-                current = remain.pop()
-                pred = graph.predecessors(current)
-                if len(set(pred) - set(current_group)) > 0:
-                    break
+def collapse_group_of_nodes(graph, max_duration, verbose=False):
+    nodes = set(graph.nodes())
+    while len(nodes) > 0:
+        root = nodes.pop()
+        root_died = graph.node[root]['died_t']
+        last_born = graph.node[root]['died_t']
+        remain = graph.successors(root)
+        current_group = [root]
 
-                succ = graph.successors(current)
-                if len(succ) > 0 and current not in current_group:
-                    tmp_succs = []
-                    for c in current_group:
-                        tmp_succs.extend(graph.successors(c))
-                    tmp_succs = set(tmp_succs) - set(current_group) - set([current])
-                    if len(tmp_succs) > 0:
-                        first_succ_born = min([graph.node[s]['born_t'] for s in tmp_succs])
+        while len(remain) > 0 and last_born - root_died < max_duration:
+            remain = sorted(remain, key=lambda x: -graph.node[x]['born_t'])
+            current = remain.pop()
+            if current in current_group:
+                continue
+
+            succ = graph.successors(current)
+            if len(succ) == 0:
+                # test if it is a line
+                valid = True
+                head = root
+                line = [root]
+                while valid and head != current:
+                    succs = graph.successors(head)
+                    if len(succs) != 1:
+                        valid = False
                     else:
-                        first_succ_born = None
+                        head = succs[0]
+                        preds = graph.predecessors(head)
+                        if len(preds) != 1:
+                            valid = False
+                        else:
+                            line.append(head)
+                if valid and head == current:
+                    # Yes, it is a line from 'root' to 'current'
+                    if verbose:
+                        print("I: Line between {root} and {current}: {line}".format(root=root, current=current, line=line))
+                    graph.condense_nodes(line[0], *line[1:])
+                    nodes.add(line[0])
+                    nodes |= set(graph.predecessors(line[0]))
+                    nodes -= set(line[1:])
+                else:
+                    if verbose:
+                        print("I: Node {root} has a branch in {current}".format(root=root, current=current))
+                break  # branches are not allowed
 
-                    tmp_last_died = max(last_died, graph.node[current]['died_t'])
-                    if first_succ_born is None or first_succ_born > tmp_last_died:
-                        last_died = tmp_last_died
-                        for s in succ:
-                            if s not in remain and s not in current_group:
-                                remain.append(s)
-                        current_group.append(current)
+            remain = list(set(remain) | set(succ))
+            current_group.append(current)
 
-                        if len(remain) == 1 and \
-                                len(current_group) > 1 and \
-                                last_died - born < max_duration:
-                            last_safe = list(current_group)
-                            pred = graph.predecessors(remain[0])
-                            if len(set(pred) - set(last_safe)) == 0:
-                                last_safe.append(remain[0])
-            if last_safe is not None:
-                candidates.append(last_safe)
+            last_born = max(last_born, graph.node[current]['born_t'])
 
-        result = []
-        candidates = sorted(candidates, key=lambda x: len(x))
-        while len(candidates) > 0:
-            current = candidates.pop()
-            result.append(current)
-            candidates = [c for c in candidates if len(set(c) & set(current)) == 0]
-        if len(result) == 0:
-            break
-        else:
-            if debug:
-                print("I: Condensing %d group of nodes" % len(result))
+            # test if it is a group that it can be collapsed
+            if len(current_group) > 1 and last_born - root_died <= max_duration:
+                # test if only two nodes are connected to outside (one using successors, the other using predecessors)
 
-            while len(result) > 0:
-                r = result.pop(0)
-                result = [a for a in result if len(set(a) & set(r)) == 0]
-                if debug:
-                    ss = []
-                    for n in r:
-                        preds = (str(a) for a in graph.predecessors(n))
-                        succs = (str(a) for a in graph.successors(n))
-                        ss.append("%d: Pred: %s, Succ: %s" % (n, ", ".join(preds), ", ".join(succs)))
-                    print("I: Group: (%s)" % ") - (".join(ss))
-                graph.condense_nodes(r[0], *r[1:])
+                valid = True
+                first_node = None
+                last_node = None
+                for c in current_group:
+                    if len(set(graph.predecessors(c)) - set(current_group)) > 0:
+                        if first_node is None:
+                            first_node = c
+                        else:
+                            valid = False
+                            break
+                    if len(set(graph.successors(c)) - set(current_group)) > 0:
+                        if last_node is None:
+                            last_node = c
+                        else:
+                            valid = False
+                            break
+
+                if first_node == last_node:
+                    valid = False
+
+                if root != first_node:
+                    valid = False
+
+                # Ok, collapse!
+                if valid:
+                    if verbose:
+                        ss = []
+                        for n in current_group:
+                            preds = (str(a) for a in graph.predecessors(n))
+                            succs = (str(a) for a in graph.successors(n))
+                            ss.append("%d: Pred: %s, Succ: %s" % (n, ", ".join(preds), ", ".join(succs)))
+                        print("I: Group: (%s)" % ") - (".join(ss))
+                    graph.condense_nodes(current_group[0], *current_group[1:])
+                    nodes.add(current_group[0])
+                    nodes |= set(graph.predecessors(current_group[0]))
+                    nodes -= set(current_group[1:])
+                    break  # collapsed, start again with another node
+                elif verbose:
+                    print("I: Not valid first: {first}, last: {last}, group: {group}".format(first=first_node, last=last_node, group=current_group))
