a9feba804c2d097829db008c83699257a4d9125b
Nick Timkovich <npt@u.northwestern.edu>
2014-09-29 11:25:49 -0500

    Fix the local settings imports, remove unused h5py imports

---
+++ code/waldo/__init__.py
@@ -0,0 +1,4 @@
+from .wio import Experiment
+from .network import Graph
+from . import collider
+#from . import viz # require separate import of vizualization functions
--- code/waldo/collider/__init__.py
+++ code/waldo/collider/__init__.py
@@ -1,5 +1,3 @@
 from __future__ import absolute_import
 
-# from . import viz # require manual import
 from .simplifications import *
-from . import subgraph
