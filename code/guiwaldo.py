a40bcbf1cca89ee05967153acf15a14c0f85675a
Heltena <heltena@gmail.com>
2015-06-01 16:29:30 -0500

    LogForWindows added. GUI is raising no errors on running.

--- code/guiwaldo.py
+++ code/guiwaldo.py
@@ -1,9 +1,22 @@
 __author__ = 'heltena'
 
+
+class LogForWindows(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.file = open(filename, 'at')
+
+    def write(self, text):
+        self.file.write(text)
+
+    def flush(self):
+        self.file.flush()
+
 import sys
-# sys.stdout = Blackhole()
-# sys.stderr = Blackhole()
-# del Blackhole
+log = LogForWindows('guiwaldo_log.txt')
+sys.stdout = log
+sys.stderr = log
+del LogForWindows
 
 import matplotlib
 matplotlib.use('Qt4Agg')
@@ -12,13 +25,6 @@ from PyQt4 import QtGui
 # import json
 from waldo.gui import WaldoApp
 
-
-class Blackhole(object):
-    def write(self, text):
-        pass
-
-    def flush(self):
-        pass
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
